fd62ea109a765523db7c8a040450068bf26ad266
Thebullfighter9 <112989632+Thebullfighter9@users.noreply.github.com>
2023-11-29 22:50:02 +0000

    Deploying to gh-pages from @ Thebullfighter9/therapy-for-a-friend@c3556ff41ed569648315bf3eb5778e3fc148080b 🚀

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from backend.db.database import init_db
 from backend.api.auth import bp as auth_bp
 from backend.api.sessions import bp as sessions_bp
@@ -8,32 +8,25 @@ from backend.api.users import bp as users_bp
 app = Flask(__name__)
 
 # Secret Key Configuration
+# For development, you can use a generated key. For production, use a secure key.
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
+# Database Initialization
+# Make sure to set the correct database URI
 # Database Initialization
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:HS3492k5$!@localhost/TFAF'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 init_db(app)
-
 # Register Blueprints for API routes
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
 app.register_blueprint(sessions_bp, url_prefix='/api/sessions')
 app.register_blueprint(users_bp, url_prefix='/api/users')
 
-# Error Handling for Debugging
-@app.errorhandler(500)
-def internal_error(error):
-    return "500 error"
+# Optional: Configure additional components like logging, error handlers, etc.
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return "404 error"
-
-# Optional: Serve Frontend Pages
 @app.route('/')
 def index():
-    return render_template('index.html')  # Ensure you have an 'index.html' in your 'templates' folder
+    return "Welcome to the Therapy for a Friend API!"
 
-# Run in Debug Mode
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # host='0.0.0.0' makes the server publicly available
+    app.run(debug=True)
